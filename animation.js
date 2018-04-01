6acddd9be7caa128101f0548c5ea0000bf6ccd9c
Mikhail <mandarson@gmail.com>
2018-04-01 18:38:46 +0300

    Second Revision

--- animation.js
+++ animation.js
@@ -3,29 +3,46 @@ $(function () { // wait for document ready
     var controller = new ScrollMagic.Controller();
 
     // build tween
-    var tween = TweenMax.to("#title", 0.1, { rotation: 320, ease: Linear.easeNone });
+    var tween = TweenMax.to("#title", 0.5, { repeat: 10, scale: 2, yoyo: true });
 
     // build scene
-    var scene = new ScrollMagic.Scene({ duration: 250 })
+    var scene = new ScrollMagic.Scene({ duration: "100%" })
         .setTween(tween)
-        .setPin("#title", { pushFollowers: false })
+        //.setPin("#title", { pushFollowers: false })
         .addTo(controller);
     
     
 
 });
-
 $(function () { // wait for document ready
     // init controller
-    let controller = new ScrollMagic.Controller();
+    var controller1 = new ScrollMagic.Controller();
 
     // build tween
-    let tween = TweenMax.to("#title", 0.1, { rotation: 320, ease: Linear.easeNone });
+    var tween1 = TweenMax.to("#wow-gif", 0.5, { scale: 1.5, repeat: 5, yoyo: true });
 
     // build scene
-    let scene = new ScrollMagic.Scene({ duration: 250 })
+    var scene = new ScrollMagic.Scene({ duration: "100%" })
+        .setTween(tween1)
+        .addTo(controller1);
+    
+    
+
+});
+$(function () { // wait for document ready
+    // init controller
+    var controller = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: "onLeave"
+        }
+    });
+
+    // build tween
+    var tween = TweenMax.to("#igor-gif", 0.1, {rotation:360 });
+
+    // build scene
+    var scene = new ScrollMagic.Scene({ duration: 360, triggerElement: "#wow-gif" })
         .setTween(tween)
-        .setPin("#title", { pushFollowers: false })
         .addTo(controller);
     
     
@@ -33,24 +50,27 @@ $(function () { // wait for document ready
 });
 
 
+
+
+/*
 $(function () { // wait for document ready
     // init
-    let controller = new ScrollMagic.Controller({
+    var controller = new ScrollMagic.Controller({
         globalSceneOptions: {
             triggerHook: 'onLeave'
         }
     });
 
     // get all slides
-    let slides = document.querySelectorAll("section.panel");
+    var slides = document.querySelectorAll("section.panel");
 
     // create scene for every slide
-    for (let i = 0; i < slides.length; i++) {
+    for (var i = 0; i < slides.length; i++) {
         new ScrollMagic.Scene({
             triggerElement: slides[i]
         })
             .setPin(slides[i])
-            .addIndicators() // add indicators (requires plugin)
             .addTo(controller);
     }
 });
+*/
